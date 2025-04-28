8c31a31a88f3d7ae79cae80fa85092dd8788f6a4
HoHyunKim-kr <hohyun1022@naver.com>
2025-04-28 17:33:12 +0900

    fix : err

--- app.py
+++ app.py
@@ -270,4 +270,4 @@ def download_file(filename):
     return send_file(os.path.join(DOWNLOADS_DIR, filename), as_attachment=True)
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5500)
